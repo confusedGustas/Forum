47f8d916b396811569afd1ebd2f59e1195498827
ugnius.spokavicius <uspokavicius@gmail.com>
2025-01-17 17:25:11 +0200

    Dao tests

--- src/test/java/org/site/forum/domain/topic/dao/TopicDaoTests.java
+++ src/test/java/org/site/forum/domain/topic/dao/TopicDaoTests.java
@@ -1,9 +1,11 @@
 package org.site.forum.domain.topic.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.site.forum.domain.topic.entity.Topic;
+import org.site.forum.domain.user.dao.UserDao;
+import org.site.forum.domain.user.dao.UserDaoImpl;
 import org.site.forum.domain.user.entity.User;
-import org.site.forum.domain.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,33 +18,59 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import(TopicDaoImpl.class)
+@Import({TopicDaoImpl.class, UserDaoImpl.class})
 public class TopicDaoTests {
 
     @Autowired
     private TopicDao topicDao;
 
     @Autowired
-    private UserRepository userRepository;
+    private UserDao userDao;
 
-    @Test
-    public void testSaveTopic() {
-        var author = User.builder()
-                .id(1L)
+    private Topic topic;
+
+    @BeforeEach
+    public void setUp() {
+        User author = User.builder()
                 .uuid(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"))
                 .build();
 
-        var topic = Topic.builder()
+        userDao.saveUser(author);
+
+        topic = Topic.builder()
                 .title("Test title")
                 .content("Test content")
                 .author(author)
                 .build();
+    }
 
+    @Test
+    public void testSaveTopic() {
         Topic savedTopic = topicDao.saveTopic(topic);
 
         assertNotNull(savedTopic.getId());
+        assertNotNull(savedTopic.getTitle());
+        assertNotNull(savedTopic.getContent());
+        assertNotNull(savedTopic.getAuthor());
         assertEquals(topic.getTitle(), savedTopic.getTitle());
         assertEquals(topic.getContent(), savedTopic.getContent());
         assertEquals(topic.getAuthor(), savedTopic.getAuthor());
     }
+
+    @Test
+    public void testGetTopic() {
+        Topic savedTopic = topicDao.saveTopic(topic);
+
+        Topic foundTopic = topicDao.getTopic(savedTopic.getId());
+
+        assertNotNull(foundTopic.getId());
+        assertNotNull(foundTopic.getTitle());
+        assertNotNull(foundTopic.getContent());
+        assertNotNull(foundTopic.getAuthor());
+        assertEquals(savedTopic.getId(), foundTopic.getId());
+        assertEquals(savedTopic.getTitle(), foundTopic.getTitle());
+        assertEquals(savedTopic.getContent(), foundTopic.getContent());
+        assertEquals(savedTopic.getAuthor(), foundTopic.getAuthor());
+    }
+
 }
