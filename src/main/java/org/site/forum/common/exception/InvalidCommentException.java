10d971c2fe83f81f5bfd32753f1e4a3e23940c27
ugnius.spokavicius <uspokavicius@gmail.com>
2025-01-25 23:33:35 +0200

    hotfix

---
+++ src/main/java/org/site/forum/common/exception/InvalidCommentException.java
@@ -0,0 +1,7 @@
+package org.site.forum.common.exception;
+
+public class InvalidCommentException extends RuntimeException {
+  public InvalidCommentException(String message) {
+    super(message);
+  }
+}
