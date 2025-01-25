cfd3951ad152513694bb9275deb434166533ba2f
confusedGustas <gustas.antanas2003@gmail.com>
2025-01-26 01:58:38 +0200

    test fix, data integrity checks for rating extracted to its own implementation, so it can be injected.

---
+++ src/main/java/org/site/forum/domain/rating/integrity/RatingDataIntegrity.java
@@ -0,0 +1,15 @@
+package org.site.forum.domain.rating.integrity;
+
+import org.site.forum.domain.rating.entity.Rating;
+import org.site.forum.domain.user.entity.User;
+import java.util.UUID;
+
+public interface RatingDataIntegrity {
+
+    void validateRatingValue(Integer ratingValue);
+    void validateRatingEntity(Rating rating);
+    void validateRatingExists(UUID topicId, UUID userId);
+    void validatePostIdAndUserId(UUID postId, UUID userId);
+    void validateUserExists(User user);
+
+}
