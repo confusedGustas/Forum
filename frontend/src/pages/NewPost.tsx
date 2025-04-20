92075bb5aa4b33cbf5e6f00c52c7ee6dca5ab8b1
Antanas4 <antanas.buteliauskas@gmail.com>
2025-04-20 16:28:31 +0300

    added more disallowed objects

--- frontend/src/pages/NewPost.tsx
+++ frontend/src/pages/NewPost.tsx
@@ -99,7 +99,7 @@ const NewPost = () => {
             if (err.response) {
                 if (err.response.status === 401) {
                     errorMessage = "You must be logged in to create a post.";
-                } else if (err.response.data.error.includes("Image file contains restricted content")) {
+                } else if (err.response.data?.error?.includes("Image file contains restricted content")) {
                     errorMessage = "One or more uploaded images were rejected due to restricted content.";
                 }
                 else {
